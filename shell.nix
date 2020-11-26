32c328c15d44d0c7a4f3a52978483d049b18094f
Markus Kohlhase <markus.kohlhase@slowtec.de>
2020-11-26 10:57:30 +0100

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rustChannel = pkgs.rustChannelOf { channel = "stable"; };
+in
+  with pkgs;
+  mkShell {
+    buildInputs = [
+      rustChannel.rust
+      clang
+      cmake
+      pkgconfig
+    ];
+    LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
+}
